a4824e62c4bf0bed3aa68ad5c2bc96c513043fb4
Tzadikvtovlo <167207218+Tzadikvtovlo@users.noreply.github.com>
2026-08-30 15:26:21 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const lastCollectedTime = document.getElementById("lastCollectedTime");
     const lastRunTime = document.getElementById("lastRunTime");
     const lastRunStatus = document.getElementById("lastRunStatus");
+    const updateBanner = document.getElementById("updateBanner"); // קריאה לבאנר
     
     const btnRun = document.getElementById("btnRun");
     const btnTrivia = document.getElementById("btnTrivia");
@@ -23,6 +24,28 @@ document.addEventListener("DOMContentLoaded", async () => {
         lastCollectedTime.textContent = storage.lastCollectedTime || "טרם בוצע";
         lastRunTime.textContent = storage.lastRunTime || "טרם בוצע";
         lastRunStatus.textContent = storage.lastRunStatus || "לא ידוע";
+
+        // הצגת הבאנר אם יש עדכון ושינוי הכיתוב למבנה של דף ההגדרות
+        if (storage.hasGitHubUpdate) {
+            updateBanner.style.display = "block";
+            
+            const updateLink = updateBanner.querySelector("a");
+            const currentVersion = chrome.runtime.getManifest().version;
+            
+            // שליפת הגרסה העדכנית מגיטהאב כדי להציג אותה בכיתוב
+            fetch("https://raw.githubusercontent.com/Tzadikvtovlo/MatbehAli/main/manifest.json")
+                .then(res => res.json())
+                .then(data => {
+                    updateLink.textContent = `יש עדכון!   מותקן: v${currentVersion} | זמין: v${data.version}`;
+                    updateLink.title = "לחץ כאן להורדה";
+                }).catch(() => {
+                    // גיבוי במקרה של שגיאת רשת
+                    updateLink.textContent = "יש גרסה חדשה! לחץ כאן להורדה";
+                });
+                
+        } else {
+            updateBanner.style.display = "none";
+        }
 
         chrome.runtime.sendMessage({ action: "getAccountInfo" }, (res) => {
             const isConnected = res?.isConnected;
